7e935dcd0230dfc01c65475975a46657a1017843
tugberkozkara <tugberkozkara@gmail.com>
2023-07-26 13:10:16 +0300

    Prepare socket server boilerplate

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {cors: {origin: 'http://localhost:3000',}});
+const port = 5000;
+
+io.on('connection', (socket) => {
+    console.log(socket.id + " connected");
+    socket.on('disconnect', () => {
+        console.log(socket.id + " disconnected");
+    });
+});
+
+
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+    }
+);
