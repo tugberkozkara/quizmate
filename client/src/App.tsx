8fd1dbea887bac6d1d3d8dd8a0281428eb3ab824
tugberkozkara <tugberkozkara@gmail.com>
2023-07-26 13:11:15 +0300

    Add socket client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { socket } from './socket';
+
 
 function App() {
+  
+  useEffect(() => {
+    socket.connect();
+    socket.on('connect', () => {
+      console.log(socket.id + ' connected');
+    });
+    socket.on('disconnect', () => {
+      console.log(socket.id + ' disconnected');
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
     </div>
   );
 }
