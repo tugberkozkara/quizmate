fba009c2c9fefdcd4bb63e9b1ce1760b3609a9c1
tugberkozkara <tugberkozkara@gmail.com>
2023-07-27 00:41:39 +0300

    Collect connected players in an array

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
+import { Player } from "./player/player";
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {cors: {origin: 'http://localhost:3000',}});
@@ -9,8 +11,15 @@ const port = 5000;
 
 io.on('connection', (socket) => {
     console.log(socket.id + " connected");
+
+    for(let [id, socket] of io.of("/").sockets) {
+        const player = new Player(id, socket.handshake.auth.username);
+        Player.addPlayer(player);
+    }
+
     socket.on('disconnect', () => {
         console.log(socket.id + " disconnected");
+        Player.removePlayer(socket.id);
     });
 });
 
