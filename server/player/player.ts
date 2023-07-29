dc9f3bea65fe57d109e6c5d0a244ce466bbd9914
tugberkozkara <tugberkozkara@gmail.com>
2023-07-29 17:53:51 +0300

    Move player methods into platform

--- server/player/player.ts
+++ server/player/player.ts
@@ -1,3 +1,4 @@
+import { Room } from '../room/room';
 
 export class Player {
     id: string;
@@ -8,19 +9,9 @@ export class Player {
         this.username = username;
     }
 
-    static addPlayer(player: Player): Player {
-        if (players.filter(e => e.id === player.id).length === 0) {
-            players.push(player);
-        }
-        return player;
-    }
-
-    static removePlayer(id: string): void {
-        const index = players.findIndex(player => player.id === id);
-        players.splice(index, 1);
+    joinRoom(room: Room): void {
+        room.addPlayer(this);
     }
 }
-
-export const players: Player[] = [];
 
 
