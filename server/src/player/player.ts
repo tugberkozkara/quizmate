acd7f0ebebd98974b51a4e9ddb49806570ff7e3a
tugberkozkara <tugberkozkara@gmail.com>
2023-07-31 18:50:46 +0300

    Move player under src/player

---
+++ server/src/player/player.ts
@@ -0,0 +1,31 @@
+import { Room } from '../room/room';
+import { Platform } from '../platform/platform';
+
+export class Player {
+    id: string;
+    username: string;
+
+    constructor(id: string, username: string) {
+        this.id = id;
+        this.username = username;
+    }
+
+    createRoom(platform: Platform): Room {
+        const room = new Room(this);
+        if (platform.rooms.filter(e => e.id === room.id).length === 0) {
+            platform.addRoom(room);
+        }
+        return room;
+    }
+
+    joinRoom(platform: Platform, roomId: string): Room | null {
+        const room = platform.rooms.filter(e => e.id === roomId)[0];
+        if (room.players.length < room.capacity) {
+            room.addPlayer(this);
+            return room;
+        }
+        return null;
+    }
+}
+
+
