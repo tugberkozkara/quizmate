35477b35f55540c5d9a6a148d1aab7ea6700677a
tugberkozkara <tugberkozkara@gmail.com>
2023-08-08 23:40:32 +0300

    Use room id naming instead of room key

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -3,9 +3,9 @@ import { socket } from "../socket";
 import { useNavigate } from 'react-router-dom';
 
 
-export default function Homepage() {
+export default function Homepage({ username }: { username: string }) {
 
-  const [roomKey, setRoomKey] = useState('');
+  const [roomId, setRoomId] = useState('');
   const navigate = useNavigate();
 
   // Create a room
@@ -15,36 +15,40 @@ export default function Homepage() {
 
 
   // Join a room
-  const roomKeyHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRoomKey(e.target.value);
+  const roomIdHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRoomId(e.target.value);
   }
 
   const joinRoomHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    socket.emit('join-room', e.currentTarget['room-key'].value);
+    socket.emit('join-room', e.currentTarget['room-id'].value);
   }
 
   useEffect(() => {
     socket.on('room-created', (room: any) => {
-      navigate(`/room/${room.id}`);
+      navigate(`/room/${room.id}`, { state: { room: room }});
     })
-    
+
     socket.on('room-joined', (room: any) => {
-      navigate(`/room/${room.id}`);
+      navigate(`/room/${room.id}`, { state: { room: room }});
     })
     
     socket.on('room-is-full', () => {
       console.log('room is full');
       navigate(`/`);
     })
-  }, []);
+  }, [navigate]);
 
 
   return (
     <section className="text-center col col-lg-3 col-md-4 col-sm-6 col-6 mx-auto">
+      <p>
+        <h1 className="h3 mb-3 fw-normal">Welcome {username}</h1>
+      </p>
+
     <form onSubmit={joinRoomHandler}>
         <div className="input-group mb-3">
-          <input type="text" className="form-control" placeholder="Enter room key" name="room-key" value={roomKey} onChange={roomKeyHandler}></input>
+          <input type="text" className="form-control" placeholder="Enter room id" name="room-id" value={roomId} onChange={roomIdHandler}></input>
         </div>
         <p className='mb-0'>
         <button className="btn btn-outline-primary my-2" type='submit'>Join the room</button>
