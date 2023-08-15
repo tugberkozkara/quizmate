113371b93741a83e2a50b970facdd878c8cc21ab
tugberkozkara <tugberkozkara@gmail.com>
2023-08-15 17:59:11 +0300

    Add finish game

--- client/src/components/QuestionCard.tsx
+++ client/src/components/QuestionCard.tsx
@@ -1,16 +1,38 @@
+import { useState } from 'react';
+
+export const QuestionCard = ({ question, nextQuestion, selfAnswers, finishGame }: { question: any, nextQuestion: any, selfAnswers: any, finishGame: ()=>void } ) => {
+
+    const [answer, setAnswer] = useState('');
+
+    const answerHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer(e.target.value);
+    }
+
+    const collectAnswer = (e: any) => {
+        e.preventDefault();
+        selfAnswers.push({questionId: question['id'], answer: answer});
+        if (e.target.id === 'finish-button') {
+            finishGame();
+        }
+    }
 
 
-export const QuestionCard = ({ question }: { question: any } ) => {
+
   return (
     <>
-        <div className="card text-center bg-secondary p-5">
+        <div className="card text-center bg-light p-5">
             <div className="card-body">
                 <h5 className="card-title">{question['id']}</h5>
                 <h6 className="card-title text-muted">{question['category']}</h6>
                 <h4 className="card-title">{question['question']}</h4>
                 <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Answer" name="answer" ></input>
+                    <input type="text" className="form-control" placeholder="Answer" name="answer" value={answer} onChange={answerHandle}></input>
                 </div>
+                {nextQuestion ?
+                    <button type="button" id="next-button" className="btn btn-outline-primary" data-bs-target="#carouselExample" data-bs-slide="next" onClick={collectAnswer}>Next Question</button>
+                    :
+                    <button type="button" id="finish-button" className="btn btn-outline-primary" onClick={collectAnswer}>Finish</button>
+                }
             </div>
         </div>
     </>
--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,5 +1,7 @@
-import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { QuestionCard } from '../components/QuestionCard';
+import { socket } from "../socket";
 
 export default function Game() {
 
@@ -9,37 +11,67 @@ export default function Game() {
         question: string,
         answer: string
     }
+
+    type selfAnswer = {
+        questionId: number,
+        answer: string
+    }
     
     const {state} = useLocation();
-    const roomId = state.room.id;
+    const room = state.room;
     const game = state.game;
     const questions: question[] = game.questions;
+    const selfAnswers: selfAnswer[] = [];
+    const navigate = useNavigate();
+    
+    const finishGame = () => {
+        console.log('finish game');
+        socket.emit('finish-game', room.id, game.id, selfAnswers);
+    }
+
+    const leaveRoom = () => {
+        socket.emit('leave-room', room.id);
+        navigate(`/`);
+    }
+
+    useEffect(() => {
+        socket.on('waiting-for-players', (room: any) => {
+            console.log('waiting for players in room ' + room.id);
+        })
+
+        socket.on('game-finished', (room: any, gameScore: any) => {
+            console.log('game finished');
+            console.log(gameScore);
+        })
+    }, [])
+    
 
   return (
     <>
-        <section className="text-center col col-lg-3 col-md-4 col-sm-6 col-6 mx-auto">
-            <div>Game {roomId}</div>
+        <section className="text-center col col-lg-6 col-md-6 col-sm-10 col-10 mx-auto">
+            <div>Game {game.id}</div>
 
             <div id="carouselExample" className="carousel slide">
-            <div className="carousel-inner">
-                    {questions.map((question: question) => {
-                        return (
-                            <div className="carousel-item active">
-                                <QuestionCard question={question}  />
-                            </div>
-                        )
-                    })}
+                <div className="carousel-inner">
+                        {questions.map((question: question, i: any) => {
+                            let nextQuestion = questions[question.id]
+                            return (
+                                question.id === 1 ?
+                                <div className="carousel-item active" key={i}>
+                                    <QuestionCard question={question} nextQuestion={nextQuestion} selfAnswers={selfAnswers} finishGame={finishGame} />
+                                </div>
+                                :
+                                <div className="carousel-item" key={i}>
+                                    <QuestionCard question={question} nextQuestion={nextQuestion} selfAnswers={selfAnswers} finishGame={finishGame} />
+                                </div>
+                            )
+                        })}
+                </div>
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-            </div>
-            
+
+            <p className='mb-0'>
+                <button className="btn btn-outline-danger my-3" onClick={leaveRoom}>Leave Game</button>
+            </p>
 
         </section>
     </>
