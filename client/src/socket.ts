4f9dd83bda97d74850688cb78e0203d8d8988e34
tugberkozkara <tugberkozkara@gmail.com>
2023-08-16 16:24:08 +0300

    Remove console.logs

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,25 +2,3 @@ import io from 'socket.io-client';
 
 const URL = "http://localhost:5000";
 export const socket = io(URL, {autoConnect: false});
-
-socket.on('connect', () => {
-    console.log(socket.id + ' connected');
-});
-
-socket.on('disconnect', () => {
-    console.log(socket.id + ' disconnected');
-});
-
-socket.on("room-created", (room: any) => {
-    console.log(room.id + "room-created");
-});
-
-socket.on("room-is-full", () => {
-    console.log("room-is-full");
-});
-
-socket.on("room-joined", (room: any) => {
-    console.log(room.id + "room-joined");
-});
-
-
