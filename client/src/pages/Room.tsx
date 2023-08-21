afff393dc313ebbf23655c154a11e8067c52b56b
tugberkozkara <tugberkozkara@gmail.com>
2023-08-21 17:12:13 +0300

    Show alert while questions are generating

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -10,6 +10,7 @@ export default function Room({ selfUsername }: { selfUsername: string }) {
     const [roomCategories, setRoomCategories] = useState(room.categories || []);
     const [category, setCategory] = useState('');
     const [waitingForPlayersAlert, setWaitingForPlayersAlert] = useState(false);
+    const [waitingForQuestionsAlert, setWaitingForQuestionsAlert] = useState(false);
     const [roomLeftUser, setRoomLeftUser] = useState('');
     const navigate = useNavigate();
 
@@ -56,8 +57,15 @@ export default function Room({ selfUsername }: { selfUsername: string }) {
             setWaitingForPlayersAlert(true);
         })
 
+        socket.on('waiting-for-questions', (room: any) => {
+            document.getElementById('start-game-button')?.classList.add('disabled');
+            setWaitingForQuestionsAlert(true);
+            setWaitingForPlayersAlert(false);
+        })
+
         socket.on('game-started', (room: any, game: any) => {
             setWaitingForPlayersAlert(false);
+            setWaitingForQuestionsAlert(false);
             navigate(`/game/${game.id}`, { state: { room: room, game: game }});
         })
 
@@ -85,45 +93,57 @@ export default function Room({ selfUsername }: { selfUsername: string }) {
                     </div>
                 }
             </div>
-            
-            <form onSubmit={addCategory}>
-                <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Enter a category" name="category" value={category} onChange={categoryHandler} required></input>
-                </div>
-                <p className='mb-0'>
-                    <button id="add-category-button" className="btn btn-outline-dark my-2" type='submit'>Add category</button>
-                </p>
-            </form>
-
-            <p className='mb-0'>
-                <button id="start-game-button" className="btn btn-primary my-2 disabled" onClick={startGame}>Start Game</button>
-            </p>
-
-            <p className='mb-0'>or</p>
-            <p className='mb-0'>
-                <button className="btn btn-outline-danger my-2" onClick={leaveRoom}>Leave Room</button>
-            </p>
 
             <div className="container mb-0 mt-5">
-                <div className="row">
-                    <div className="col">
-                    <p className='mb-0 mt-5'>players: </p>
-                        {roomPlayers.map((player: any, i: any) => (
-                            player.username === selfUsername ?
-                            <p className='mb-0 text-primary' key={i}>{player.username}</p>
-                            :
-                            <p className='mb-0' key={i}>{player.username}</p>
-                        ))}
+                {waitingForQuestionsAlert ?(
+                    <div className="alert alert-warning" role="alert">
+                        <h4 className="alert-heading">Waiting for Questions</h4>
+                        <p>AI generated questions will be here!</p>
+                        <div className="spinner-border" role="status"></div>
+                        <p className="mb-0 small text-muted">This process may take up to a minute.</p>
                     </div>
-                    <div className="col">
-                    <p className='mb-0 mt-5'>categories: </p>
-                        {roomCategories.map((cat: string, i: any) => (
-                            <p className='mb-0' key={i}>{cat}</p>
-                        ))}
-                    </div>
-                </div>
+                ):
+                (
+                    <>
+                        <form onSubmit={addCategory}>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Enter a category" name="category" value={category} onChange={categoryHandler} required></input>
+                            </div>
+                            <p className='mb-0'>
+                                <button id="add-category-button" className="btn btn-outline-dark my-2" type='submit'>Add category</button>
+                            </p>
+                        </form>
+
+                        <p className='mb-0'>
+                            <button id="start-game-button" className="btn btn-primary my-2 disabled" onClick={startGame}>Start Game</button>
+                        </p>
+
+                        <p className='mb-0'>or</p>
+                        <p className='mb-0'>
+                            <button className="btn btn-outline-danger my-2" onClick={leaveRoom}>Leave Room</button>
+                        </p>
+
+                        <div className="container mb-0 mt-5">
+                            <div className="row">
+                                <div className="col">
+                                <p className='mb-0 mt-5'>players: </p>
+                                    {roomPlayers.map((player: any, i: any) => (
+                                        player.username === selfUsername ?
+                                        <p className='mb-0 text-primary' key={i}>{player.username}</p>
+                                        :
+                                        <p className='mb-0' key={i}>{player.username}</p>
+                                    ))}
+                                </div>
+                                <div className="col">
+                                <p className='mb-0 mt-5'>categories: </p>
+                                    {roomCategories.map((cat: string, i: any) => (
+                                        <p className='mb-0' key={i}>{cat}</p>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </>)}
             </div>
-        
         </section>
     </>
   )
