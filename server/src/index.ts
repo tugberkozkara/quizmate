110eeb37eac3eafdbd6ed19c51dcf57a71c4a45f
tugberkozkara <tugberkozkara@gmail.com>
2023-08-29 20:06:00 +0300

    Use env vars

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
 
 import { Platform } from "./platform/platform";
 import { socketRouter } from "./routers/socketRouter";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {cors: {origin: 'http://localhost:3000',}});
-const port = 5000;
+const io = new Server(server, { cors: { origin: process.env.CORS_ORIGIN, } });
+const PORT = process.env.PORT || 5000;
 
 const platform = new Platform();
 
@@ -18,7 +20,7 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(port, () => {
-    console.log(`listening on *:${port}`);
-    }
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+}
 );
