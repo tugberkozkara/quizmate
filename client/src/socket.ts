78946ef8befe8d326613594fcb9dfff2e2fe0646
tugberkozkara <tugberkozkara@gmail.com>
2023-09-04 19:01:42 +0300

    Use env var for socket url

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
-const URL = "http://localhost:5000";
+const URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:5000";
 export const socket = io(URL, {autoConnect: false});
