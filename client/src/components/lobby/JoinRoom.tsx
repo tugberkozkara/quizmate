9037b00126f1c8a2b0b49a0caf9deea9553cf34d
tugberkozkara <tugberkozkara@gmail.com>
2023-09-05 16:28:42 +0300

    Update room id input as number

--- client/src/components/lobby/JoinRoom.tsx
+++ client/src/components/lobby/JoinRoom.tsx
@@ -17,10 +17,10 @@ export const JoinRoom = () => {
     <div className="text-center align-self-center">
     <form onSubmit={joinRoom}>
         <div className="input-group mb-3">
-          <input type="text" className="form-control" placeholder="Enter room id" name="room-id" value={roomId} onChange={roomIdHandler} required></input>
+          <input type="number" className="form-control" placeholder="Enter room id" name="room-id" value={roomId} onChange={roomIdHandler} required></input>
         </div>
         <p className='mb-0'>
-        <button className="btn btn-outline-primary my-2" type='submit'>Join the room</button>
+          <button className="btn btn-outline-primary my-2" type='submit'>Join the room</button>
         </p>
     </form>
     </div>
